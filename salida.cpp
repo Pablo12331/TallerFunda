92bdbf0b85f3f4b618fd4a2dcae189134647332e
Pablo12331 <130071914+Pablo12331@users.noreply.github.com>
2025-06-17 23:15:41 +0000

    a

---
+++ salida.cpp
@@ -0,0 +1,13 @@
+// Código generado por mi compilador
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main() {
+    int x = 5;
+    float y = 1.5;
+    float z = (x + y);
+    std::cout << z << std::endl;
+    return 0;
+}
